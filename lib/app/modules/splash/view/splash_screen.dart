cecd13a375e230046a9c143498cc274de7b34399
Faqihasyari <faqih8158@gmail.com>
2025-06-02 21:05:30 +0700

    splash screen

---
+++ lib/app/modules/splash/view/splash_screen.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+
+import 'package:app_lms/app/routes/app_pages.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Delay selama 3 detik, lalu pindah ke halaman berikutnya
+    Timer(Duration(seconds: 3), () {
+      Get.offAllNamed(Routes.HOME);  // Ganti dengan rute tujuanmu
+    });
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset(
+          'assets/images/logo/logo1.png', // ganti dengan path logo kamu
+          width: 150,
+          height: 150,
+        ),
+      ),
+    );
+  }
+}
--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,20 +1,22 @@
 import 'package:get/get.dart';
-
-import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
+import '../modules/splash/view/splash_screen.dart';
 
 part 'app_routes.dart';
 
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.HOME;
+  static const INITIAL = Routes.SPLASH;
 
   static final routes = [
     GetPage(
+      name: _Paths.SPLASH,
+      page: () => const SplashScreen(),
+    ),
+    GetPage(
       name: _Paths.HOME,
       page: () => const HomeView(),
-      binding: HomeBinding(),
     ),
   ];
 }
