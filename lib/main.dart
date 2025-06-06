9c93e33da36862fcbf527654999aae4576ef6be5
Faqihasyari <faqih8158@gmail.com>
2025-06-06 14:53:37 +0700

    deletefirebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
-import 'package:app_lms/firebase_options.dart';
-import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+void main() {
+  
   runApp(
     GetMaterialApp(
       title: "Application",
