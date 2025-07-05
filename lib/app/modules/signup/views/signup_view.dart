d06a2e9f32f5b5e93a05bdbdfda54796b872538d
Faqihasyari <faqih8158@gmail.com>
2025-07-05 20:02:45 +0700

    signup

--- lib/app/modules/signup/views/signup_view.dart
+++ lib/app/modules/signup/views/signup_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../controllers/signup_controller.dart';
 
@@ -9,16 +10,28 @@ class SignupView extends GetView<SignupController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('SignupView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'SignupView is working',
-          style: TextStyle(fontSize: 20),
+        appBar: AppBar(
+          title: Column(
+            children: [
+              Text(
+                'Sign Up',
+                style: GoogleFonts.plusJakartaSans(
+                    fontWeight: FontWeight.w600, fontSize: 32),
+              ),
+            ],
+          ),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Text(
+                'Create an account to begin your Learning Journey',
+                style: GoogleFonts.plusJakartaSans(fontSize: 15),
+              ),
+            )
+          ],
+        ));
   }
 }
