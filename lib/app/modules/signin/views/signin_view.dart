beda61a19c925a7fcf2bcf22a513bc3885898326
Faqihasyari <faqih8158@gmail.com>
2025-07-05 19:46:53 +0700

    fix overflow

--- lib/app/modules/signin/views/signin_view.dart
+++ lib/app/modules/signin/views/signin_view.dart
@@ -20,106 +20,110 @@ class SigninView extends GetView<SigninController> {
           },
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Sign in',
-            style: GoogleFonts.plusJakartaSans(
-              fontWeight: FontWeight.w600,
-              fontSize: 32,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Sign in',
+              style: GoogleFonts.plusJakartaSans(
+                fontWeight: FontWeight.w600,
+                fontSize: 32,
+              ),
             ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text(
-            'Please Sign in with your account',
-            style: GoogleFonts.plusJakartaSans(fontSize: 16),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(30, 60, 30, 35),
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: 'Email Here',
-                labelStyle: GoogleFonts.plusJakartaSans(),
-                floatingLabelStyle: TextStyle(color: black),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: ring),
-                  borderRadius: BorderRadius.circular(10), // Sudut membulat
-                ),
-                contentPadding:
-                    EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: ring),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Please Sign in with your account',
+              style: GoogleFonts.plusJakartaSans(fontSize: 16),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(30, 60, 30, 35),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Email Here',
+                  labelStyle: GoogleFonts.plusJakartaSans(),
+                  floatingLabelStyle: TextStyle(color: black),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: ring),
+                    borderRadius: BorderRadius.circular(10), // Sudut membulat
+                  ),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: ring),
+                  ),
                 ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30),
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: 'Password',
-                labelStyle: GoogleFonts.plusJakartaSans(),
-                floatingLabelStyle: TextStyle(color: black),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: ring),
-                  borderRadius: BorderRadius.circular(10), // Sudut membulat
-                ),
-                contentPadding:
-                    EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: ring),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  labelStyle: GoogleFonts.plusJakartaSans(),
+                  floatingLabelStyle: TextStyle(color: black),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: ring),
+                    borderRadius: BorderRadius.circular(10), // Sudut membulat
+                  ),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: ring),
+                  ),
                 ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 55,
-          ),
-          SizedBox(
-            height: 53,
-            width: 300,
-            child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: btn,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    )),
-                onPressed: () {},
-                child: Text(
-                  'SIGN IN',
-                  style: GoogleFonts.plusJakartaSans(
-                      fontWeight: FontWeight.w600, fontSize: 20, color: white),
-                )),
-          ),
-          SizedBox(
-            height: 80,
-          ),
-          SizedBox(
-            height: 53,
-            width: 300,
-            child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: white,
-                    shape: RoundedRectangleBorder(
+            SizedBox(
+              height: 55,
+            ),
+            SizedBox(
+              height: 53,
+              width: 300,
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: btn,
+                      shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(10),
-                        side: BorderSide(color: ring, width: 1))),
-                onPressed: () {},
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Image.asset('assets/images/logo/google_icon.png'),
-                    Text(
-                      'Sign In with Google',
-                      style: GoogleFonts.plusJakartaSans(
-                          fontSize: 20, color: black),
-                    ),
-                  ],
-                )),
-          )
-        ],
+                      )),
+                  onPressed: () {},
+                  child: Text(
+                    'SIGN IN',
+                    style: GoogleFonts.plusJakartaSans(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 20,
+                        color: white),
+                  )),
+            ),
+            SizedBox(
+              height: 80,
+            ),
+            SizedBox(
+              height: 53,
+              width: 300,
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: white,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          side: BorderSide(color: ring, width: 1))),
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Image.asset('assets/images/logo/google_icon.png'),
+                      Text(
+                        'Sign In with Google',
+                        style: GoogleFonts.plusJakartaSans(
+                            fontSize: 20, color: black),
+                      ),
+                    ],
+                  )),
+            )
+          ],
+        ),
       ),
     );
   }
