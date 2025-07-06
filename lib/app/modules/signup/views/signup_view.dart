c9551641f5496561c0f5a3925d09e220290f0b1f
Faqihasyari <faqih8158@gmail.com>
2025-07-06 22:13:14 +0700

    signup

--- lib/app/modules/signup/views/signup_view.dart
+++ lib/app/modules/signup/views/signup_view.dart
@@ -1,3 +1,4 @@
+import 'package:app_lms/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -30,7 +31,34 @@ class SignupView extends GetView<SignupController> {
                 'Create an account to begin your Learning Journey',
                 style: GoogleFonts.plusJakartaSans(fontSize: 15),
               ),
-            )
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(30, 60, 30, 35),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Full Name'),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Your Name Here',
+                      hintStyle: TextStyle(color: hint),
+                      labelStyle: GoogleFonts.plusJakartaSans(),
+                      floatingLabelStyle: TextStyle(color: black),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: ring),
+                        borderRadius:
+                            BorderRadius.circular(10), // Sudut membulat
+                      ),
+                      contentPadding:
+                          EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: ring),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ));
   }
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -5,4 +5,7 @@ Color white = Color(0xffffffff);
 Color black = Color(0xff040404);
 Color side = Color(0xff003096);
 Color ring = Color(0xffD9D9D9);
+Color hint = Color(0xffB5B5B5);
+
+
 
