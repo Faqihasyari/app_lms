be70bbe7b97f8ec750b6bb314901451f60e3f36e
Faqihasyari <faqih8158@gmail.com>
2025-07-17 23:34:23 +0700

    push

--- lib/app/modules/detail_overview/views/detail_overview_view.dart
+++ lib/app/modules/detail_overview/views/detail_overview_view.dart
@@ -1,3 +1,4 @@
+import 'package:app_lms/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -11,23 +12,48 @@ class DetailOverviewView extends GetView<DetailOverviewController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          centerTitle: true,
-          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.bookmark))],
-        ),
-        body: ListView.builder(
-          itemCount: 1,
-          itemBuilder: (context, index) {
-            final course = Get.arguments as Map<String, dynamic>;
-            return Column(
-              children: [
-                Image.network(
-                  course['image_url'] ?? '',
-                )
-              ],
-            );
-          },
-        ));
+        extendBodyBehindAppBar: true,
+        body: Stack(children: [
+          ListView.builder(
+            itemCount: 1,
+            itemBuilder: (context, index) {
+              final course = Get.arguments as Map<String, dynamic>;
+              return Column(
+                children: [
+                  Image.network(
+                    course['image_url'] ?? '',
+                  )
+                ],
+              );
+            },
+          ),
+          Positioned(
+              top: MediaQuery.of(context).padding.top,
+              left: 0,
+              right: 0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      Get.back();
+                    },
+                    icon: Icon(
+                      Icons.arrow_back,
+                      color: black,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      Get.back();
+                    },
+                    icon: Icon(
+                      Icons.bookmark,
+                      color: black,
+                    ),
+                  ),
+                ],
+              ))
+        ]));
   }
 }
